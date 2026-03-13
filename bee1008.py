4d4226c7078c3711e6c7b7ab1eaa959c33bb45e8
Lucas Francelino de Pontes <lucasfrancelinopontes@gmail.com>
2026-03-13 14:21:07 -0300

    feat: resoluções
    1006-1008

---
+++ bee1008.py
@@ -0,0 +1,8 @@
+nun_funcionario = int(input())
+horas_trabalhadas = int(input())
+valor_hora = float(input())
+
+salario = horas_trabalhadas * valor_hora
+
+print(f"NUMBER = {nun_funcionario}")
+print(f"SALARY = U$ {salario:.2f}")
